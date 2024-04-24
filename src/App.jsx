e0ed22b61d23c8a75034ef9c24a8f4a970252e9c
Tankiso Montse <tmontse16@gmail.com>
2024-04-25 01:53:48 +0200

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+// import { useState } from 'react';
+import PropTypes from 'prop-types';
+
 
 function App() {
   return <div className="fullApp">
+    <Header appName="Currency Web App" />
   </div>;
 }
 
+const Header = (props) => {
+  Header.propTypes = {
+    appName: PropTypes.string.isRequired // appName should be a required string
+  };
+
+  return (
+    <div className='header'>
+      <h1>{props.appName}</h1>
+    </div>
+  );
+};
 
 export default App
